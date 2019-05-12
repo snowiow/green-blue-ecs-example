4ac2720e7ee1e04e8e562be05d4a4b96eba4255b
snowiow <marcel.patzwahl+github@posteo.de>
2019-05-12 22:59:12 +0200

    codedeploy

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -51,6 +51,24 @@ data "aws_iam_policy_document" "pipeline" {
       "${aws_codebuild_project.this.arn}",
     ]
   }
+
+  statement {
+    sid    = "AllowCodeDeploy"
+    effect = "Allow"
+
+    actions = [
+      "codedeploy:CreateDeployment",
+      "codedeploy:GetApplication",
+      "codedeploy:GetApplicationRevision",
+      "codedeploy:GetDeployment",
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:RegisterApplicationRevision",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "pipeline" {
@@ -153,10 +171,145 @@ resource "aws_codebuild_project" "this" {
     image           = "aws/codebuild/docker:18.09.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
+
+    environment_variable {
+      name  = "REPOSITORY_URI"
+      value = "${data.aws_ecr_repository.this.repository_url}"
+    }
+
+    environment_variable {
+      name  = "CONTAINER_NAME"
+      value = "${local.container_name}"
+    }
+
+    environment_variable {
+      name  = "SUBNET_1"
+      value = "${aws_subnet.this.*.id[0]}"
+    }
+
+    environment_variable {
+      name  = "SUBNET_2"
+      value = "${aws_subnet.this.*.id[1]}"
+    }
+
+    environment_variable {
+      name  = "SUBNET_3"
+      value = "${aws_subnet.this.*.id[2]}"
+    }
+
+    environment_variable {
+      name  = "SECURITY_GROUP"
+      value = "${aws_security_group.ecs.id}"
+    }
   }
 
   source {
     type = "CODEPIPELINE"
+  }
+}
+
+data "aws_iam_policy_document" "assume_by_codedeploy" {
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name               = "codedeploy"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_by_codedeploy.json}"
+}
+
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    sid    = "AllowLoadBalancingAndECSModifications"
+    effect = "Allow"
+
+    actions = [
+      "ecs:CreateTaskSet",
+      "ecs:DeleteTaskSet",
+      "ecs:DescribeServices",
+      "ecs:UpdateServicePrimaryTaskSet",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:DescribeRules",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:ModifyListener",
+      "elasticloadbalancing:ModifyRule",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowS3"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codedeploy" {
+  role   = "${aws_iam_role.codedeploy.name}"
+  policy = "${data.aws_iam_policy_document.codedeploy.json}"
+}
+
+resource "aws_codedeploy_app" "this" {
+  compute_platform = "ECS"
+  name             = "example-deploy"
+}
+
+resource "aws_codedeploy_deployment_group" "this" {
+  app_name              = "${aws_codedeploy_app.this.name}"
+  deployment_group_name = "example-deploy-group"
+  service_role_arn      = "${aws_iam_role.codedeploy.arn}"
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  ecs_service {
+    cluster_name = "${aws_ecs_cluster.this.name}"
+    service_name = "${aws_ecs_service.this.name}"
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = ["${aws_lb_listener.this.arn}"]
+      }
+
+      target_group {
+        name = "${aws_lb_target_group.this.*.name[0]}"
+      }
+
+      target_group {
+        name = "${aws_lb_target_group.this.*.name[1]}"
+      }
+    }
   }
 }
 
@@ -199,10 +352,30 @@ resource "aws_codepipeline" "this" {
       provider         = "CodeBuild"
       version          = "1"
       input_artifacts  = ["source"]
-      output_artifacts = ["appspec"]
+      output_artifacts = ["build"]
 
       configuration {
         ProjectName = "${aws_codebuild_project.this.name}"
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeployToECS"
+      version         = "1"
+      input_artifacts = ["build"]
+
+      configuration {
+        ApplicationName                = "${aws_codedeploy_app.this.name}"
+        DeploymentGroupName            = "${aws_codedeploy_deployment_group.this.deployment_group_name}"
+        TaskDefinitionTemplateArtifact = "build"
+        AppSpecTemplateArtifact        = "build"
       }
     }
   }
