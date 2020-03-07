64e6fb9b908a20981d82dcd15ce28f27b9f8b545
Kris(DevOps) <kris.membe@gmail.com>
2020-03-07 19:41:57 +0200

    Breaking the boundries

--- app/main_test.go
+++ app/main_test.go
@@ -11,7 +11,7 @@ func Test_fileHandler(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
 	fh.ServeHTTP(w, req)
-	if w.Code != http.StatusOK {
+	if w.Code == http.StatusOK {
 		t.Errorf("Home page didn't return %v", http.StatusOK)
 	}
 }
